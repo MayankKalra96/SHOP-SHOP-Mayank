5c237312ab480bd0cd7f7f04df31649f9fc25833
MayankKalra96 <112035006+MayankKalra96@users.noreply.github.com>
2023-02-23 15:32:06 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# SHOP-SHOP-Mayank
+## Description 
+
+Shop Shop is an e-commerce site in which you can purchase products and checkout using stripe. This application is refactor of the original which used React’s Context API to handle global state. Shop Shop Redux uses the [Redux](https://redux.js.org/) library to handle global state.
+
+## User Story
+
+`AS a senior engineer working on an e-commerce platform`<br/>
+`I WANT my platform to use Redux to manage global state instead of the Context API`<br/>
+`SO THAT my website's state management is taken out of the React ecosystem`
+
+### Acceptance Criteria
+
+`GIVEN an e-commerce platform that uses Redux to manage global state` <br/>
+`WHEN I review the app’s store` <br/>
+`THEN I find that the app uses a Redux store instead of the Context API` <br/>
+`WHEN I review the way the React front end accesses the store` <br/>
+`THEN I find that the app uses a Redux provider` <br/>
+`WHEN I review the way the app determines changes to its global state` <br/>
+`THEN I find that the app passes reducers to a Redux store instead of using the Context API` <br/>
+`WHEN I review the way the app extracts state data from the store`<br/>
+`THEN I find that the app uses Redux instead of the Context API`<br/>
+`WHEN I review the way the app dispatches actions`<br/>
+`THEN I find that the app uses Redux instead of the Context API`<br/>
+
+## Walkthrough
+
+
+Drive: https://drive.google.com/file/d/1UupSxpTHubj1RQKElfsmHa_0-YmiZAyK/view?usp=share_link
+
+
+## Website
+
+heroku app: 
+
+github repo:
---
+++ server/config/seeds.js
@@ -0,0 +1,155 @@
+const db = require('./connection');
+const { User, Product, Category } = require('../models');
+
+db.once('open', async () => {
+  await Category.deleteMany();
+
+  const categories = await Category.insertMany([
+    { name: 'Food' },
+    { name: 'Household Supplies' },
+    { name: 'Electronics' },
+    { name: 'Books' },
+    { name: 'Toys' }
+  ]);
+
+  console.log('categories seeded');
+
+  await Product.deleteMany();
+
+  const products = await Product.insertMany([
+    {
+      name: 'Tin of Cookies',
+      description:
+        'a tin of cookies.',
+      image: 'cookie-tin.jpg',
+      category: categories[0]._id,
+      price: 2.99,
+      quantity: 500
+    },
+    {
+      name: 'Canned Coffee',
+      description:
+        'canned coffee',
+      image: 'canned-coffee.jpg',
+      category: categories[0]._id,
+      price: 1.99,
+      quantity: 500
+    },
+    {
+      name: 'Toilet Paper',
+      category: categories[1]._id,
+      description:
+        'tp',
+      image: 'toilet-paper.jpg',
+      price: 7.99,
+      quantity: 20
+    },
+    {
+      name: 'Handmade Soap',
+      category: categories[1]._id,
+      description:
+        'handmade soap',
+      image: 'soap.jpg',
+      price: 3.99,
+      quantity: 50
+    },
+    {
+      name: 'Set of Wooden Spoons',
+      category: categories[1]._id,
+      description:
+        'wood spoon',
+      image: 'wooden-spoons.jpg',
+      price: 14.99,
+      quantity: 100
+    },
+    {
+      name: 'Camera',
+      category: categories[2]._id,
+      description:
+        'cam',
+      image: 'camera.jpg',
+      price: 399.99,
+      quantity: 30
+    },
+    {
+      name: 'Tablet',
+      category: categories[2]._id,
+      description:
+        'tablet',
+      image: 'tablet.jpg',
+      price: 199.99,
+      quantity: 30
+    },
+    {
+      name: 'Tales at Bedtime',
+      category: categories[3]._id,
+      description:
+        'book',
+      image: 'bedtime-book.jpg',
+      price: 9.99,
+      quantity: 100
+    },
+    {
+      name: 'Spinning Top',
+      category: categories[4]._id,
+      description: 'Lorem ipsum dolor sit amet',
+      image: 'spinning-top.jpg',
+      price: 1.99,
+      quantity: 1000
+    },
+    {
+      name: 'Set of Plastic Horses',
+      category: categories[4]._id,
+      description:
+        '(hehe)',
+      image: 'plastic-horses.jpg',
+      price: 2.99,
+      quantity: 1000
+    },
+    {
+      name: 'Teddy Bear',
+      category: categories[4]._id,
+      description:
+        'girl bestie',
+      image: 'teddy-bear.jpg',
+      price: 7.99,
+      quantity: 100
+    },
+    {
+      name: 'Alphabet Blocks',
+      category: categories[4]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'alphabet-blocks.jpg',
+      price: 9.99,
+      quantity: 600
+    }
+  ]);
+
+  console.log('products seeded');
+
+  await User.deleteMany();
+
+  await User.create({
+    firstName: 'Mayank',
+    lastName: 'Kalra',
+    email: 'mayankkalra96@gmail.com',
+    password: 'password12345',
+    orders: [
+      {
+        products: [products[0]._id, products[0]._id, products[1]._id]
+      }
+    ]
+  });
+
+  await User.create({
+    firstName: 'Apple',
+    lastName: 'Boy',
+    email: 'appleboy@testmail.com',
+    password: 'password12345'
+  });
+
+  console.log('users seeded');
+
+  process.exit();
+});
